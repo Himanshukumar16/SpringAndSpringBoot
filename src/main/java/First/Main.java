5e2dfd2ab72f03256d2c56aefe113b713be6631c
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-07-11 20:32:04 +0530

    Added_pom.xml_concept.

--- src/main/java/First/Main.java
+++ src/main/java/First/Main.java
@@ -1,7 +1,25 @@
 package First;
-
 class Main{
     public static void main(String[] args) {
         System.out.println("hello");
+/*
+    XML terms :->
+        groupID Should be unique.
+        ArtifactId is the project Name.
+        and version is the version of dependencies....
+
+    we can add dependencies here by using dependencies tag....
+    and connect to any dependency.... like shown below:
+
+    <dependencies>
+        <dependency>
+            <groupId>com.mysql</groupId>
+            <artifactId>mysql-connector-j</artifactId>
+            <version>9.3.0</version>
+        </dependency>
+    </dependencies>
+
+
+ */
     }
 }
