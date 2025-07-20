7fda3169a815eb6d9ffe6a67efb1a54b907c5a42
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-07-21 00:07:12 +0530

    Structured.

--- src/main/java/XML_Concept.java
+++ src/main/java/XML_Concept.java
@@ -1,2 +1,24 @@
 public class XML_Concept {
+    /*
+    XML terms :->
+        groupID Should be unique.
+        ArtifactId is the project Name.
+        and version is the version of dependencies....
+
+    we can add dependencies here by using dependencies tag....
+    and connect to any dependency.... like shown below:
+
+    <dependencies>
+        <dependency>
+            <groupId>com.mysql</groupId>
+            <artifactId>mysql-connector-j</artifactId>
+            <version>9.3.0</version>
+        </dependency>
+    </dependencies>
+
+    In Maven, an archetype is basically a template for generating a new project.
+
+    Maven completes here !!....
+ */
+
 }
