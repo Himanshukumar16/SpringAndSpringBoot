3c748ce3b0658cf106b9ddd892f8be816c8859c2
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-07-26 21:50:31 +0530

    Hibernate_Added

--- src/main/Code/Main.java
+++ src/main/Code/Main.java
@@ -1,5 +1,3 @@
-
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -8,9 +6,9 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Students s1 = new Students();
-        s1.setSid(4);
-        s1.setSname("Chanchala");
-        s1.setSgpa(8.70);
+        s1.setSid(6);
+        s1.setSname("Sibu");
+        s1.setSgpa(7.90);
 
         Configuration con = new Configuration();
         con.addAnnotatedClass(Students.class);
@@ -18,7 +16,10 @@ public class Main {
         SessionFactory sf = con.buildSessionFactory();
         Session s = sf.openSession();
         Transaction tr = s.beginTransaction();
-        s.save(s1);
+        s.persist(s1);
         tr.commit();
+
+        s.close();
+        sf.close();
     }
 }
