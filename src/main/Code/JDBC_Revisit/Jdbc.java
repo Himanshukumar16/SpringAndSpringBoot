97dc48efb3d4af79518e915e4b1d411ef36d8a5c
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-10-04 23:41:08 +0530

    Revisiting_jdbc.

--- src/main/Code/JDBC_Revisit/Jdbc.java
+++ src/main/Code/JDBC_Revisit/Jdbc.java
@@ -1,4 +1,17 @@
 package JDBC_Revisit;
+import java.sql.*;
 
 public class Jdbc {
+    public static void main(String[] args) {
+
+        String url = "jdbc:postgresql://localhost:5433/First";
+        try {
+            Connection con = DriverManager.getConnection(url,"postgres","1234");
+            System.out.println("connection established !");
+        } catch (SQLException e) {
+            System.out.println(e);
+            System.out.println("Something went wrong in connecting with database !");
+        }
+
+    }
 }
