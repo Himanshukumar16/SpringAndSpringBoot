3694410fe1bd4ba3828b1d57b33ddaa33b59b556
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-10-04 23:54:26 +0530

    Revisiting_jdbc.

--- src/main/Code/JDBC_Revisit/Jdbc.java
+++ src/main/Code/JDBC_Revisit/Jdbc.java
@@ -5,13 +5,21 @@ public class Jdbc {
     public static void main(String[] args) {
 
         String url = "jdbc:postgresql://localhost:5433/First";
+        String sql = "SELECT * FROM imp_info;";
         try {
             Connection con = DriverManager.getConnection(url,"postgres","1234");
             System.out.println("connection established !");
+            Statement st = con.createStatement();
+            ResultSet r = st.executeQuery(sql);
+            while(r.next()) {
+                System.out.print(r.getInt(1) + " ");
+                System.out.print(r.getString(2)+" ");
+                System.out.print(r.getString(3));
+                System.out.println();
+            }
         } catch (SQLException e) {
             System.out.println(e);
             System.out.println("Something went wrong in connecting with database !");
         }
-
     }
 }
