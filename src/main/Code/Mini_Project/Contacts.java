c80c72102bffd7a76e1e966eb697b42bddf0ccb7
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-10-06 23:27:09 +0530

    Mini_Project.

--- src/main/Code/Mini_Project/Contacts.java
+++ src/main/Code/Mini_Project/Contacts.java
@@ -1,25 +1,15 @@
 package Mini_Project;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Contacts {
-    @Id
-    private int id;
+
     @Column(name = "name of person")
     private String name;
+    @Id
     @Column(name = "number of person")
     private String num;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
--- src/main/Code/Mini_Project/Main.java
+++ src/main/Code/Mini_Project/Main.java
@@ -4,7 +4,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
+import java.util.List;
 import java.util.Scanner;
 
 // mini project for practice -> Contact book.
@@ -24,19 +26,25 @@ public class Main {
 
         while(true) {
 
-            System.out.println("Enter the Choice : ");
+            System.out.println("Enter the Choice/Number : ");
             System.out.println("1) for Searching any number");
             System.out.println("2) for Adding any number");
             System.out.println("3) for Deleting any number");
             System.out.println("4) Exit");
             int n = sc.nextInt();
             if(n == 1) {
+                System.out.println("Please Enter Name in small letters only.");
                 System.out.println("Enter Name of Person : ");
                 String name = sc.next();
-
+                Query<Contacts> q = s.createQuery("FROM Contacts WHERE name = :man", Contacts.class);
+                q.setParameter("man",name);
+                List<Contacts> l = q.list();
+                if (l.isEmpty()) System.out.println("No Record Found.");
+                for(Contacts i : l) System.out.println("Number of "+ name +" is : " + i.getNum());
             }
             if(n == 2){
                 Contacts c = new Contacts();
+                System.out.println("Please Enter Name in small letters only.");
                 System.out.println("Enter Name of Person : ");
                 String name = sc.next();
                 c.setName(name);
@@ -45,9 +53,19 @@ public class Main {
                 c.setNum(num);
                 s.persist(c);
             }
-            if(n == 3) {}
-            if(n == 4) break;
-            t.commit();
+            if(n == 3) {
+                System.out.println("Please Enter Name in small letters only.");
+                System.out.println("Enter Name of Person : ");
+                String name = sc.next();
+                Query<?> q = s.createQuery("DELETE FROM Contacts WHERE name = :man");
+                q.setParameter("man",name);
+                System.out.println("Number Deleted.");
+
+            }
+            if(n == 4) {
+                t.commit();
+                break;
+            }
             if(n < 1 || n > 4) System.out.println("Wrong input !");
         }
         s.close();
